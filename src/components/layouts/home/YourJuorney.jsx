f6df0e0c7dce6ba233fcb4e14ac5061d31c64229
MD Motasim Billah <themotasimdev@gmail.com>
2026-02-22 19:30:24 +0600

    your journey responsive running

--- src/components/layouts/home/YourJuorney.jsx
+++ src/components/layouts/home/YourJuorney.jsx
@@ -10,18 +10,18 @@ import P18 from "../../P18";
 const YourJuorney = () => {
   return (
     <>
-      <section className="lg:py-30 bg-secondary">
+      <section className="py-15 lg:py-30 bg-secondary">
         <Container>
-          <h4 className="lg:text-[32px] font-semibold text-white">
-            Your path into global universities
+          <h4 className="text-lg lg:text-[32px] font-semibold text-white">
+            Your path into global universities -
           </h4>
           <p className="lg:text-2xl text-white font-semibold">
             We’ve helped 50,000+ international students get into university.
           </p>
           {/* ========================== */}
-          <div className="relative mt-8 overflow-hidden pb-30">
-            <div className="absolute -top-37 left-116 py-135 px-29.5 -rotate-60 bg-linear-to-b from-[#d5d5ff7a] to-[#d5d5ff] z-0"></div>
-            <div className="relative lg:w-60 lg:h-60 rounded-full bg-yellow-100 z-10">
+          <div className="relative mt-8 overflow-hidden pb-70 lg:pb-30">
+            <div className="absolute lg:-top-37 lg:left-116 py-47 md:py-53 rounded-full lg:rounded-none lg:py-135 px-8.5 lg:px-29.5 lg:-rotate-60 bg-linear-to-b from-[#d5d5ff7a] to-[#d5d5ff] z-0"></div>
+            <div className="relative w-17 h-17 md:w-40 md:h-40 lg:w-60 lg:h-60 rounded-full bg-yellow-100 z-10">
               <div className="w-full h-full rounded-full overflow-hidden">
                 <img
                   src={journeyOne}
@@ -30,9 +30,9 @@ const YourJuorney = () => {
                 />
               </div>
             </div>
-            <div className="z-10 max-w-120 absolute top-0 left-90">
-              <p className="lg:text-[32px] font-semibold text-white">Step 1</p>
-              <h3 className="lg:text-6xl text-primary font-extrabold pr-5">
+            <div className="z-10 max-w-120 absolute top-0 left-20 lg:left-90">
+              <p className="text-2xl lg:text-[32px] font-semibold text-white">Step 1</p>
+              <h3 className="text-4xl md:text-5xl lg:text-6xl text-primary font-extrabold pr-5">
                 Choose your pathway
               </h3>
               <P18
@@ -41,25 +41,25 @@ const YourJuorney = () => {
                 }
                 className={"text-white pr-5 py-4"}
               />
-              <Button className={""}>Our Programmes</Button>
+              <Button className={"bg-blue-200! lg:bg-secondary"}>Our Programmes</Button>
             </div>
-            <div className="relative my-78 flex justify-end">
-              <div className="z-10 max-w-120 absolute top-0 left-0">
-                <p className="lg:text-[32px] font-semibold text-white">
+            <div className="relative my-60 lg:my-78 flex lg:justify-end">
+              <div className="z-10 max-w-120 absolute top-0 left-20 lg:left-0">
+                <p className="text-2xl lg:text-[32px] font-semibold text-white">
                   Step 2
                 </p>
-                <h3 className="lg:text-6xl text-primary font-extrabold pr-19">
+                <h3 className="text-4xl md:text-5xl lg:text-6xl text-primary font-extrabold lg:pr-19">
                   Support with every step
                 </h3>
                 <P18
                   text={
                     "Our expert student support teams will guide you through every step of your journey and help find the best university and course for you."
                   }
-                  className={"text-white pr-21 py-4"}
+                  className={"text-white lg:pr-21 py-4"}
                 />
                 <Button className={"bg-blue-200!"}>How we support you</Button>
               </div>
-              <div className="relative lg:w-60 lg:h-60 rounded-full bg-yellow-100 z-10">
+              <div className="relative w-17 h-17 md:w-40 md:h-40 lg:w-60 lg:h-60 rounded-full bg-yellow-100 z-10">
                 <div className="w-full h-full rounded-full overflow-hidden">
                   <img src={journeyTwo} alt="" />
                 </div>
@@ -67,8 +67,8 @@ const YourJuorney = () => {
             </div>
             {/* ================================= */}
             <div className="relative">
-              <div className="absolute -bottom-34 left-121 py-134 px-29.5 rotate-60 bg-linear-to-b from-[#d5d5ff7a] to-[#d5d5ff] z-0"></div>
-              <div className="relative lg:w-60 lg:h-60 rounded-full bg-yellow-100 z-10">
+              <div className="absolute bottom-0 lg:-bottom-34 lg:left-121 rounded-full lg:rounded-none py-47 md:py-53 lg:py-134 px-8.5 lg:px-29.5 lg:rotate-60 bg-linear-to-b from-[#d5d5ff7a] to-[#d5d5ff] z-0"></div>
+              <div className="relative w-17 h-17 md:w-40 md:h-40 lg:w-60 lg:h-60 rounded-full bg-yellow-100 z-10">
                 <div className="w-full h-full rounded-full overflow-hidden">
                   <img
                     src={journeyThree}
@@ -77,11 +77,11 @@ const YourJuorney = () => {
                   />
                 </div>
               </div>
-              <div className="z-10 max-w-121 absolute top-0 left-90">
-                <p className="lg:text-[32px] font-semibold text-white">
+              <div className="z-10 max-w-121 absolute top-0 left-20 lg:left-90 ">
+                <p className="text-2xl lg:text-[32px] font-semibold text-white">
                   Step 3
                 </p>
-                <h3 className="lg:text-6xl text-primary font-extrabold">
+                <h3 className="text-4xl md:text-5xl lg:text-6xl text-primary font-extrabold">
                   Gain entry to your dream university
                 </h3>
                 <P18
