fd4ba87050ef88dc3ad5a4cdc563364b5e17cf7f
MD Motasim Billah <themotasimdev@gmail.com>
2026-02-27 23:40:26 +0600

    blog page

--- src/components/layouts/blog/BlogCards.jsx
+++ src/components/layouts/blog/BlogCards.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Container from "../../Container";
 import CardBlog from "../../CardBlog";
 import blogs from "../../../data/blog";
 
 const BlogCards = () => {
+  const [search, setSearch] = useState("");
+
+  // filter blogs by title
+  const filteredBlogs = useMemo(() => {
+    return blogs.filter((item) =>
+      item.title.toLowerCase().includes(search.toLowerCase()),
+    );
+  }, [search]);
   return (
     <>
       <section className="py-11 lg:py-20 ">
         <Container>
+          <div className="relative md:w-1/2 m-auto md:mb-10">
+            {/* <label className="text-gray-500 md:pl-3">Search by University</label> */}
+            <input
+              onChange={(e)=>setSearch(e.target.value)}
+              type="search"
+              placeholder="Search blog"
+              className="border border-gray-300 rounded-xl px-4 py-2 w-full focus:outline-none"
+            />
+          </div>
+          {/* ==================== */}
           <div className="grid grid-cols-1 md:grid-cols-2 md:justify-between lg:grid-cols-3 gap-y-10">
-            {blogs?.map((item) => (
+            {filteredBlogs?.map((item) => (
               <CardBlog
                 key={item.id}
                 id={item.id}
